b3d768d72ef8e25e133902ecbcbcbdf291fb64e6
JIEUNNN <pandaa0628@gmail.com>
2022-01-31 13:57:12 +0900

    first commit

---
+++ dbmovie.py
@@ -0,0 +1,10 @@
+from pymongo import MongoClient
+client = MongoClient('mongodb+srv://test:sparta@cluster0.afaoe.mongodb.net/Cluster0?retryWrites=true&w=majority')
+db = client.dbsparta
+
+movie = db.movies.find_one({'title':'가버나움'})
+star = movie['star']
+
+all_movies = list(db.movies.find({'star': star},{'_id':False}))
+for m in all_movies:
+    print(m['title'])
